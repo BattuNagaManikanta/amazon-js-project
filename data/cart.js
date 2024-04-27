fe68cf58b33ddafa6c8f3bb67368b7569bbe73c1
manikanta <maniking22000@gmail.com>
2024-04-27 18:25:55 +0530

    save the data and generate html for payment summary

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,5 @@
 export let cart=JSON.parse(localStorage.getItem('cart'))||[{
-    productId:"83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity:2,
     deliveryOptionId:'1'
 },{
